fbfc5b2a0ff4db069ffeb606a2ff1a4ca18c50e6
Felix Steiner <felix_steiner@bluewin.ch>
2024-12-20 14:36:50 +0100

    style(debounce): fix linting errors

--- packages/tools/src/debounce.unit.mjs
+++ packages/tools/src/debounce.unit.mjs
@@ -5,21 +5,21 @@ test('waits with execution', async (t) => {
     let called = 0;
     const call = () => called++;
     const debouncedCall = debounce(call, 5);
-    debouncedCall()
+    debouncedCall();
     t.is(called, 0);
-    await new Promise(resolve => setTimeout(resolve, 5));
+    await new Promise((resolve) => setTimeout(resolve, 5));
     t.is(called, 1);
 });
 
 test('debounce re-starts timer', async (t) => {
-    const call = () => called++;
-    const debouncedCall = debounce(call, 5);
     let called = 0;
+    const call = () => { called += 1; };
+    const debouncedCall = debounce(call, 5);
     debouncedCall();
-    await new Promise(resolve => setTimeout(resolve, 3));
+    await new Promise((resolve) => setTimeout(resolve, 3));
     debouncedCall();
-    await new Promise(resolve => setTimeout(resolve, 3));
+    await new Promise((resolve) => setTimeout(resolve, 3));
     debouncedCall();
-    await new Promise(resolve => setTimeout(resolve, 5));
+    await new Promise((resolve) => setTimeout(resolve, 5));
     t.is(called, 1);
 });
