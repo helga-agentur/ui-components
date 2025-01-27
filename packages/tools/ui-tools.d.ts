7046fcff685ac4bbe18d592292712b0bf4bb567d
Andi <andi@joinbox.ch>
2025-01-27 17:51:55 +0100

    feat(typescript support): add type declarations for ui-tools
    https://github.com/helga-agentur/drupal-theme-customer/issues/118

---
+++ packages/tools/ui-tools.d.ts
@@ -0,0 +1,36 @@
+declare module '@helga-agency/ui-tools' {
+    interface ValidationOptions<T> {
+        transform?: (value: string) => T;
+        validate?: (value: T) => boolean;
+        expectation?: string;
+    }
+
+    export function readAttribute<T>(
+        element: HTMLElement,
+        attributeName: string,
+        options: ValidationOptions<T>,
+    ): T;
+
+    export function debounce(
+        callback: () => void,
+        offset: number,
+    ): () => void;
+
+    export function once(
+        element: HTMLElement,
+        name: string,
+        callback: () => void,
+    ): void;
+
+    interface MeasurementOptions {
+        element: HTMLElement;
+        updateOnIntersection?: boolean;
+    }
+
+    interface Dimensions extends Record<string, unknown> {
+        update: () => void,
+        destroy: () => void,
+    }
+
+    export function measureElement(options: MeasurementOptions): Dimensions;
+}
